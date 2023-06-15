8f8d46d3f0ec49614fd4fb77c964c7df1249669c
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-06-15 23:08:38 +0530

    Create Sort 0 1 2.cpp

---
+++ Sort 0 1 2.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h> 
+
+void sort012(int *arr, int n)
+
+{
+
+   int c0 = 0, c1 = 0;
+
+   for(int i = 0; i < n; i++){
+
+      if(arr[i] == 0)   c0++;
+
+      else if(arr[i] == 1) c1++;
+
+   }
+
+   for(int i = 0; i < n; i++){
+
+      if(i < c0)
+
+         arr[i] = 0;
+
+      else if(i < c0 + c1)
+
+         arr[i] = 1;
+
+      else
+
+         arr[i] = 2;
+
+   }
+
+}
