f63fa82dda0a53dda3a9d9136054a918ed1c2803
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-06-22 21:37:01 +0530

    Create Cut Into Segments.cpp

---
+++ Cut Into Segments.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+// int solve(int n, int x, int y, int z, vector<int> &dp)
+// {
+// 	//base case
+// 	if(n==0)
+// 	{
+// 		return 0;
+// 	}
+// 	if(n<0)
+// 	{
+// 		return INT_MIN;
+// 	}
+// 	if(dp[n]!=-1)
+// 	{
+// 		return dp[n];
+// 	}
+// 	int a =solve(n-x,x,y,z,dp)+1;
+// 	int b=solve(n-y,x,y,z,dp)+1;
+// 	int c=solve(n-z,x,y,z,dp)+1;
+// 	dp[n]=max(a,max(b,c));
+// 	return dp[n];
+// }
+int solveTab(int n, int x, int y, int z)
+{
+	vector<int>dp(n+1,INT_MIN);
+	dp[0]=0;
+	for (int i=1;i<=n;i++)
+	{
+		if(i-x >=0 && dp[i-x] !=-1)
+		{
+			dp[i]=max(dp[i],dp[i-x]+1);
+
+		}
+		if(i-y >=0 && dp[i-y] !=-1)
+		{
+			dp[i]=max(dp[i],dp[i-y]+1);
+
+		}
+		if(i-z >=0 && dp[i-z] !=-1)
+		{
+			dp[i]=max(dp[i],dp[i-z]+1);
+
+		}
+		
+	}
+	if(dp[n]<0)
+	{
+		return 0;
+	}
+	else
+	{
+		return dp[n];
+	}
+
+
+}
+
+int cutSegments(int n, int x, int y, int z) {
+	// int ans=solve(n,x,y,z);
+    //     if (ans < 0) {
+    //       return 0;
+    //     }
+    //     return ans;
+	int ans=solveTab(n,x,y,z);
+	if(ans<0)
+	{
+		return 0;
+	}
+	return ans;
+}
