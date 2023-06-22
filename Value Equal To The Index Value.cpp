a10362f84c9b16602d3d31a283c7c07c852ac56d
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-06-22 22:36:39 +0530

    Create Value Equal To The Index Value.cpp

---
+++ Value Equal To The Index Value.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+vector<int> valueEqualToIndices(vector<int> &numArray, int n) {
+	vector<int> ans;
+	for(int i=0;i<n;i++)
+	{
+		if(numArray[i]==i)
+		{
+			ans.push_back(i);
+		}
+	}
+	if(ans.size()==0)
+	{
+		ans.push_back(-1);
+	}
+	return ans;
+}
