6caa63947bc9a382ba9d4fbddb23c20814f876e5
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-06-23 23:06:22 +0530

    Create Roman Numeral To Integer.cpp

---
+++ Roman Numeral To Integer.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+int romanToInt(string s) {
+    if(s.length()==0)
+    {
+        return 0;
+    }
+    unordered_map<char, int> m;
+   m['I']=1;
+   m['V']=5;
+   m['X']=10;
+   m['L']=50;
+   m['C']=100;
+   m['D']=500;
+   m['M']=1000;
+    int result = 0;
+    for(int i=0;i<s.length();i++)
+    {
+        if(m[s[i]]<m[s[i+1]])
+        {
+            result -=m[s[i]];
+        }
+        else
+        {
+            result +=m[s[i]];
+        }
+    }
+    return result;
+}
