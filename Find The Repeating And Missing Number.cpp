a146472e2cc3e0576eb9bf21750bfbb96f5bc9fd
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-06-26 22:38:00 +0530

    Update Find The Repeating And Missing Number.cpp

--- Find The Repeating And Missing Number.cpp
+++ Find The Repeating And Missing Number.cpp
@@ -16,4 +16,41 @@ vector<int> findRepeatingAndMissingNumbers(vector<int> nums)
         }
     }
     return v;
+    //second approach using xor(^)
+    int res = 0;
+
+   for(int i = 0 ; i<nums.size();i++){
+       res = res^nums[i];
+   }
+   for (int i = 1; i <= nums.size(); i++) {
+     res = res^i;
+   }
+
+   int bit = res & ~(res -1);
+
+   int x = 0,y=0;
+   for(int i = 0 ; i<nums.size() ;i++){
+       if(nums[i]&bit){
+           x=x^nums[i];
+       }else{
+         y = y ^ nums[i];
+       }
+   }
+   for (int i = 1; i <= nums.size(); i++) {
+     if (i & bit) {
+       x = x ^ i;
+     } else {
+       y = y ^ i;
+     }
+   }
+
+
+   for(int i = 0 ; i<nums.size();i++){
+    if(nums[i]==x){
+        return {x,y};
+    }
+  
+   }
+
+   return {y,x};
 }
