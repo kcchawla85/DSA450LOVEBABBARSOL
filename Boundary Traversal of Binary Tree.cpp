4a40bf19dc52d970444a3e51da94b611b3c98fd3
Kunal chawla <73349039+kcchawla85@users.noreply.github.com>
2023-07-19 22:25:50 +0530

    Create Boundary Traversal of Binary Tree.cpp

---
+++ Boundary Traversal of Binary Tree.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the Binary Tree node structure:
+    
+    template <typename T>
+    class TreeNode {
+        public :
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~TreeNode() {
+            if(left)
+                delete left;
+            if(right)
+                delete right;
+        }
+    };
+
+************************************************************/
+void traverseLeft(TreeNode<int>* root, vector<int> &ans) {
+        //base case
+        if( (root == NULL) || (root->left == NULL && root->right == NULL) )
+            return ;
+            
+        ans.push_back(root->data);
+        if(root->left)
+            traverseLeft(root->left, ans);
+        else
+            traverseLeft(root->right, ans);
+            
+    }
+    
+    void traverseLeaf(TreeNode<int>* root, vector<int> &ans) {
+        //base case
+        if(root == NULL)
+            return ;
+            
+        if(root->left == NULL && root->right == NULL) {
+            ans.push_back(root->data);
+            return;
+        }
+        
+        traverseLeaf(root->left, ans);
+        traverseLeaf(root->right, ans);
+        
+    }
+    
+    void traverseRight(TreeNode<int>* root, vector<int> &ans) {
+        //base case
+        if( (root == NULL) || (root->left == NULL && root->right == NULL) )
+            return ;
+        
+        if(root->right)
+            traverseRight(root->right, ans);
+        else
+            traverseRight(root->left, ans);
+            
+        //wapas aagye
+        ans.push_back(root->data);
+            
+    }
+vector<int> traverseBoundary(TreeNode<int>* root){
+    vector<int> ans;
+        if(root == NULL)
+            return ans;
+            
+        ans.push_back(root->data);
+        
+        //left part print/store
+        traverseLeft(root->left, ans);
+        
+        //traverse Leaf Nodes
+        
+        //left subtree
+        traverseLeaf(root->left, ans);
+        //right subtree
+        traverseLeaf(root->right, ans);
+        
+        //traverse right part
+        traverseRight(root->right, ans);
+        
+        return ans;
+}
+// link : https://www.youtube.com/watch?v=s1d8UGDCCN8&t=83s
